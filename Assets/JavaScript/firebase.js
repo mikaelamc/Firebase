6019c3a4a41f23b9e7b958c2c556b749a99b5b2f
Mikaela Mckee <mikaelamckee@iMac.local>
2018-07-28 09:25:29 -0500

    Third Commit

--- Assets/JavaScript/firebase.js
+++ Assets/JavaScript/firebase.js
@@ -57,10 +57,10 @@ database.ref().on("child_added", function(childSnapshot) {
   var trainTime = childSnapshot.val().time;
   var frequency = childSnapshot.val().frequency;
 
-  // Employee Info
-  console.log(trainName);
+  // Train Info
+  console.log(name);
   console.log(destination);
-  console.log(trainTime);
+  console.log(time);
   console.log(frequency);
 
 
@@ -78,34 +78,34 @@ database.ref().on("child_added", function(childSnapshot) {
 
 
 
-//    // Assumptions
-//    var tFrequency = 3;
+    // Assumptions
+   var tFrequency = 3;
 
-//    // Time is 3:30 AM
-//    var firstTime = "03:30";
+   // Time is 3:30 AM
+   var firstTime = "03:30";
 
-//    // First Time (pushed back 1 year to make sure it comes before current time)
-//    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-//    console.log(firstTimeConverted);
+   // First Time (pushed back 1 year to make sure it comes before current time)
+   var firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+   console.log(firstTimeConverted);
 
-//    // Current Time
-//    var currentTime = moment();
-//    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+   // Current Time
+   var currentTime = moment();
+   console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-//    // Difference between the times
-//    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-//    console.log("DIFFERENCE IN TIME: " + diffTime);
+   // Difference between the times
+   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+   console.log("DIFFERENCE IN TIME: " + diffTime);
 
-//    // Time apart (remainder)
-//    var tRemainder = diffTime % tFrequency;
-//    console.log(tRemainder);
+   // Time apart (remainder)
+   var tRemainder = diffTime % tFrequency;
+   console.log(tRemainder);
 
-//    // Minute Until Train
-//    var tMinutesTillTrain = tFrequency - tRemainder;
-//    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+   // Minute Until Train
+   var tMinutesTillTrain = tFrequency - tRemainder;
+   console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-//    // Next Train
-//    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-//    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+   // Next Train
+   var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+   console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
  });
